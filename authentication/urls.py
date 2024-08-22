a796050208c29f9b800b915560bf9824d9386950
syedfaaiz88 <faaiz290302@gmail.com>
2024-08-22 23:49:58 +0500

    Add Refresh Token Api

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import SignupView, LoginView, VerifyEmailView
+from .views import CustomTokenRefreshView, SignupView, LoginView, VerifyEmailView
 
 urlpatterns = [
     path('signup/', SignupView.as_view(), name='signup'),
     path('login/', LoginView.as_view(), name='login'),
     path('verify-email/<str:token>/', VerifyEmailView.as_view(), name='verify-email'),
+    path('token/refresh/', CustomTokenRefreshView.as_view(), name='token_refresh'),
 ]
